4dc675f0d57e190e9a92e877589556f75aa940f7
yangjingquan <yangjq_1991@163.com>
2026-08-02 21:30:11 +0800

    todo list sort

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig(({ mode }) => ({
   plugins: [vue()],
   base: mode === 'desktop' ? './' : '/',
-  server: { port: 5173, host: '0.0.0.0', proxy: { '/api': 'http://localhost:8100' } }
+  server: { port: 5174, host: '0.0.0.0', proxy: { '/api': 'http://localhost:8100' } }
 }))
